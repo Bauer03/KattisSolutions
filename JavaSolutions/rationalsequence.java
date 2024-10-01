161a01f50c2c2812255554e92baa91ed4f6f3ceb
Paul Bauer <Paulalexisbauer04@gmail.com>
2024-10-01 11:39:49 -0400

    Update rationalsequence.java
    solution to rationalsequence

--- JavaSolutions/rationalsequence.java
+++ JavaSolutions/rationalsequence.java
@@ -3,36 +3,27 @@ import java.util.Scanner;
 public class rationalsequence {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numtests = sc.nextInt();
-
         sc.useDelimiter(" |\n|/");
-        int i = 0;
-        while(i < numtests) {
-            int counterid = sc.nextInt();
-            int first = sc.nextInt();
-            int second = sc.nextInt();
-            System.out.println(counterid + " " + getlen(first,second));
-            i++;
+        int numtests = sc.nextInt();
+        for(int i = 0; i < numtests; i++) {
+            int id = sc.nextInt();
+            int p = sc.nextInt();
+            int q = sc.nextInt();
+            if(p == 1 && q == 1){
+                System.out.println(id+" 1/2");
+            }
+            else if(p < q){ // left
+                int newP = q;
+                int newQ = q-p;
+                System.out.println(id+" "+newP+"/"+newQ);
+            }
+            else { // right
+                int tmp = p;
+                int newP = q;
+                int newQ = q + (q * (tmp / q)) - (tmp % q);
+                System.out.println(id+" "+newP+"/"+newQ);
+            }
         }
         sc.close();
-    }
-
-    public static String getlen(int f, int s) {
-        if (f > s) return "" + s + "/" + (s-f);
-        else if (s == 1) return "1/" + (f+1);
-        int counter = 0;
-
-        while(f < s) {
-            f = (s - f);
-            counter++;
-        }
-        int temp = f;
-        f = s;
-        s = temp;
-        while(counter > 0) {
-            s = (f + s);
-            counter --;
-        }
-        return "" + f + "/" + s;
     }
 }
