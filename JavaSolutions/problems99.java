7bb8387a14322381c626d7d7d9d86c502f656a03
Paul Bauer <Paulalexisbauer04@gmail.com>
2024-10-03 16:47:38 -0400

    Update problems99.java
    solve problems99 thanks to claeb

--- JavaSolutions/problems99.java
+++ JavaSolutions/problems99.java
@@ -5,11 +5,10 @@ public class problems99 {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         sc.close();
-        
-        if(n < 99) System.out.println(99);
-        else if(n == 10000) System.out.println(9999);
-        else if (n >= 999 && n % 99 < 50) System.out.println(100 * ((n/100) +1) -1);
-        else if ((n % 99) >= 50) System.out.println(100 * (n/100) + 99);
-        else System.out.println((100 * (n/100)) -1);
+        int base = (int)Math.floor(n/100)*100;
+        int low = base-1;
+        int high = base+99;
+        if(high-n <= n-low || low < 0) System.out.println(high);
+        else System.out.println(low);
     }
 }
