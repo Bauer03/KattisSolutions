ac4ab573c813b7c3861f7acd5f14c53f519bfaa1
Paul Bauer <Paulalexisbauer04@gmail.com>
2024-10-06 13:04:45 -0400

    Update g2048.java
    work on 2048

--- JavaSolutions/g2048.java
+++ JavaSolutions/g2048.java
@@ -15,7 +15,7 @@ public class g2048 {
         if(nextMove == 0) {
             for(int i = 0; i < 4; i++) for(int j = 3; j > 0; j--) { // do NOT forget that this is a nested loop;
                 if(board[i][j-1] != 0 && (board[i][j] == board[i][j-1])) { // if cell to left has num & they're equal, combine
-                board[i][j-1] *= 2;
+                    board[i][j-1] *= 2;
                     if(board[i][j + 1] !=0 && j<3){
                         board[i][j] = board[i][j+1];
                         board[i][j+1] = 0;
@@ -26,11 +26,22 @@ public class g2048 {
                 }
             }
         } else if(nextMove == 1) { // move up
-            // i don't think I need to iterate through any differently? should just be able to look at stuff
-            // with regards to i and i-1 or i+1, depending on what exactly I'm looking for.
-            for(int i = 0; i < 3; i++) {
+            for(int i = 3; i > 0; i--) {
                 for(int j = 0; j < 4; j++) {
-                    if(board[i][j] == 0) board[i][j] = board[i+1][j];
+                    if(board[i-1][j] == 0) {
+                        board[i-1][j] = board[i][j];
+                        if(i < 3 && board[i+1][j] != 0) {
+                            board[i][j] = board[i+1][j];
+                            board[i+1][j] = 0;
+                        } else board[i][j] = 0;
+                    } else if (board[i-1][j] != 0 && board[i-1][j] == board[i][j]) {
+                        board[i-1][j] *= 2;
+                        board[i][j] = 0;
+                        if(board[i+1][j] != 0 && i<3) {
+                            board[i][j] = board[i+1][j];
+                            board[i+1][j] = 0;
+                        }
+                    }
                 }
             }
         }
